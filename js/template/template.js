3939d4ae63df10cd5cb8a1a2a371439abfe73c74
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-07 18:36:26 +0700

    add config, controller, template for register

---
+++ js/template/template.js
@@ -0,0 +1,9 @@
+// template js
+
+//url register 
+export let URLRegister = `https://asia-southeast2-gis-project-401902.cloudfunctions.net/backend-ai/registers`
+
+//url login 
+export let URLLogin = `https://asia-southeast2-gis-project-401902.cloudfunctions.net/backend-ai/login`
+
+export let token = 'token';
