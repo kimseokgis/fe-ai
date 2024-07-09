1ff8fce14ba3670cf26303a818e671332f2e8e74
harisriyoni3 <1214047@std.ulbi.ac.id>
2024-07-09 23:57:55 +0700

    - Function; Refactor Code & File

--- js/template/template.js
+++ js/template/template.js
@@ -1,9 +1,2 @@
-// template js
-
-//url register 
 export let URLRegister = `https://asia-southeast2-gis-project-401902.cloudfunctions.net/backend-ai/registers`
-
-//url login 
-export let URLLogin = `https://asia-southeast2-gis-project-401902.cloudfunctions.net/backend-ai/login`
-
 export let token = 'token';
