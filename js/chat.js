b7d69731beff27c47a63487646d5ab26b1154c0f
rofinafiin <rofinafiisr@gmail.com>
2024-07-18 08:56:59 +0700

    remove unused function

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,3 @@
-
-const form = document.getElementById("chat")
 // Function to check if a cookie exists
 function getCookie(name) {
     let cookieArr = document.cookie.split(";");
@@ -25,43 +23,4 @@ function checkLoginStatus() {
         loginButton.style.display = "block";
         logoutButton.style.display = "none";
     }
-}
-
-// Run the check on page load
-document.addEventListener("DOMContentLoaded", checkLoginStatus);
-
-form.addEventListener('submit', async function(event) {
-    event.preventDefault();
-
-    // Get value from chat-input
-    const messages = GetValue("chat-input");
-
-    try {
-        // Send POST request using Axios
-        const response = await axios.post(
-            `https://asia-southeast2-gis-project-401902.cloudfunctions.net/backend-ai/chatRegexp?key=${messages}`,
-            {},
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'login': getCookie('user_login') // Assuming getCookie function retrieves the 'user_login' cookie
-                },
-                withCredentials: true // Ensures cookies are included in the request
-            }
-        );
-
-        // Handle successful response
-        console.log('Response:', response.data);
-    } catch (error) {
-        // Handle error
-        console.error('There has been a problem with your Axios operation:', error);
-        // You can add specific error handling here
-    }
-});
-
-
-
-function GetValue(id) {
-    value = document.getElementById(id).value;
-    return value
 }
