d94aa8fbf66fa187b32e86345f40fe62899d3121
rofinafiin <rofinafiisr@gmail.com>
2024-07-19 09:04:07 +0700

    set responses jika response false

--- js/script.js
+++ js/script.js
@@ -63,10 +63,17 @@ const getChatResponse = async (incomingChatDiv) => {
     // Send POST request to API, get response and set the reponse as paragraph element text
     try {
         const response = await (await fetch(API_URL, requestOptions)).json();
-        pElement.textContent = response.responses;
+        // pElement.textContent = response.responses;
+        if (response.status == false){
+            pElement.textContent = "Aduh aduh aduhaaai aku ga ngerti nih, coba nanya yang lain biar kamu ngertiin akuu....";
+        } else {
+            pElement.textContent = response.responses; 
+        }
     } catch (error) { // Add error class to the paragraph element and set error text
         pElement.classList.add("error");
-        pElement.textContent = "Helloo";
+        if (response.status == false){
+            pElement.textContent = "Aduh aduh aduhaaai aku ga ngerti nih, coba nanya yang lain biar kamu ngertiin akuu....";
+        }
     }
 
     // Remove the typing animation, append the paragraph element and save the chats to local storage
