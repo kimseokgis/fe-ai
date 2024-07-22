db8792076d9feaee36d2ef3529f7f128ca942433
Farhan Rizki <farhanrizki101010@gmail.com>
2024-07-23 04:30:56 +0700

    update js logout js

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,6 @@
 import { deleteCookie, getCookie } from './cookieUtils.js';
 
 document.getElementById('logoutButton').addEventListener('click', function () {
-
     Swal.fire({
         title: 'Apakah Anda yakin ingin keluar?',
         icon: 'warning',
@@ -20,6 +19,7 @@ document.getElementById('logoutButton').addEventListener('click', function () {
                 confirmButtonText: 'OK'
             }).then((result) => {
                 if (result.isConfirmed) {
+                    localStorage.removeItem('username'); // Tambahkan ini untuk menghapus localStorage
                     window.location.href = 'chat.html'; // Redirect to the login page or homepage
                 }
             });
@@ -31,10 +31,18 @@ window.onload = function () {
     const userToken = getCookie('user_login');
     if (userToken) {
         document.getElementById('logoutButton').style.display = 'block';
+        const username = localStorage.getItem('username');
+        if (username) {
+            const defaultTextContainer = document.querySelector('.default-text');
+            defaultTextContainer.innerHTML = `<h1>Selamat Datang, ${username}</h1>`;
+        }
     } else {
         // Tampilkan pesan di halaman bahwa pengguna perlu login
         const chatContainer = document.querySelector(".chat-container");
         // Opsi: sembunyikan elemen input chat
         document.querySelector('.typing-container').style.display = 'none';
+
+        const defaultTextContainer = document.querySelector('.default-text');
+        defaultTextContainer.innerHTML = `<h1>Seterah Bot</h1><p>Selamat Datang di Sehat Sejahtera ChatBot</p>`;
     }
 };
