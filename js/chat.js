4f54aa172265888232bd6c80455a99e56d77cd1c
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-23 23:47:44 +0700

    add profiles js file for chat

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,4 @@
-// Check Cookies
+// Function to get cookie by name
 function getCookie(name) {
     let cookieArr = document.cookie.split(";");
     for (let i = 0; i < cookieArr.length; i++) {
@@ -15,28 +15,27 @@ function checkLoginStatus() {
     const userLogin = getCookie("user_login");
     const loginButton = document.querySelector(".sidebar-buttons .sidebar-btn");
     const logoutButton = document.querySelector("#logoutButton");
+    const profilePicture = document.querySelector("#profile-picture");
 
     if (userLogin) {
         loginButton.style.display = "none";
         logoutButton.style.display = "block";
+        profilePicture.style.display = "block";
+        profilePicture.src = localStorage.getItem('profilePicture'); // Set profile picture from localStorage
     } else {
         loginButton.style.display = "block";
         logoutButton.style.display = "none";
+        profilePicture.style.display = "none";
+        profilePicture.src = ""; // Clear profile picture
     }
 }
 
-// chat.js
-
+// DOMContentLoaded event to set welcome message and check login status
 document.addEventListener('DOMContentLoaded', () => {
     const username = localStorage.getItem('username');
     if (username) {
         const defaultTextContainer = document.querySelector('.default-text');
         defaultTextContainer.innerHTML = `<h1>Selamat Datang, ${username}</h1>`;
     }
-});
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
     checkLoginStatus();
 });
