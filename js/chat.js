f9253b7c14db69439263c9ff776cb6698d7ce41f
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-27 23:53:13 +0700

    undo deleted js

--- js/chat.js
+++ js/chat.js
@@ -29,3 +29,13 @@ function checkLoginStatus() {
         profilePicture.src = ""; // Clear profile picture
     }
 }
+
+// DOMContentLoaded event to set welcome message and check login status
+document.addEventListener('DOMContentLoaded', () => {
+    const username = localStorage.getItem('username');
+    if (username) {
+        const defaultTextContainer = document.querySelector('.default-text');
+        defaultTextContainer.innerHTML = `<h1>Selamat Datang, ${username}</h1>`;
+    }
+    checkLoginStatus();
+});
