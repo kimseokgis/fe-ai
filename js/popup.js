4416025c8e7f356e0e6973a66eb22dad5657159c
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-28 01:36:38 +0700

    update popup js for popup profile

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
-document.getElementById('close-settings-btn').addEventListener('click', function() {
-    document.getElementById('settings-popup').style.display = 'none';
-});
-
 window.addEventListener('click', function(event) {
+    if (event.target === document.getElementById('settings-popup')) {
+        document.getElementById('settings-popup').style.display = 'none';
+    }
+});
