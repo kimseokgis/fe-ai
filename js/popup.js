06ed9301b08bb65cdfaafc355ec5109055e30f67
Daffa Audya P <daffaaudyapramana03@gmail.com>
2024-07-28 22:28:35 +0700

    delete unused popup js

--- js/popup.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('click', function(event) {
-    if (event.target === document.getElementById('settings-popup')) {
-        document.getElementById('settings-popup').style.display = 'none';
-    }
-});
